322401314e45fec630ffc5b14d4f1933afbce8a2
Jose <joseimascherpa@gmail.com>
2025-04-06 13:36:20 -0300

    Add MainScreen with FlatList and button to open form modal

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,10 +1,88 @@
 
-import { Text, View } from 'react-native';
+
+import { Text, StyleSheet, View, StatusBar, FlatList, Pressable } from 'react-native';
+
+import { useUserMood } from '../hooks/useMoodStore';
+import { MoodModal } from '../components/MoodModal';
+import { CardMood } from '../components/CardMood';
 
 export const MainScreen = () => {
+
+  const { moods, toggleModal } = useUserMood();
   return (
-    <View>
-      <Text>MainScreen</Text>
+
+    <View style={ style.container }>
+      <MoodModal />
+      <View style={ style.containerTitle }>
+        <Text style={ { fontSize: 50 } } >BIENESAPP</Text>
+      </View>
+      <View style={ style.containerFlatlist }>
+        <FlatList
+          keyExtractor={ ( item ) => item.id.toString() }
+          data={ moods }
+          renderItem={ ( { item } ) =>
+            <CardMood mood={ item.mood } sleepQuality={ item.sleepQuality } note={ item.note } date={ item.date } />
+          }
+          style={ style.flatlist }
+        />
+      </View>
+      <View style={ style.containerButton }>
+        <Pressable onPress={ () => toggleModal() } style={ style.button }>
+          <Text> Agregar Nuevo Mood</Text>
+        </Pressable>
+      </View>
     </View>
+
   );
 };
+
+const style = StyleSheet.create( {
+  container: {
+    flex: 1,
+    width: "100%",
+    alignItems: 'center',
+    paddingTop: StatusBar.currentHeight,
+  },
+  containerTitle: {
+    backgroundColor: "red",
+    width: "100%",
+    flex: 0.2,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  containerFlatlist: {
+    flex: 0.7,
+    alignContent: "center",
+    alignItems: "center",
+    backgroundColor: "green",
+    width: "100%",
+    height: "100%",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  flatlist: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "red",
+    backgroundColor: "green",
+    width: "90%",
+    height: "80%",
+    alignContent: "center",
+  },
+  containerButton: {
+    flex: 0.1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  button: {
+    width: "70%",
+    height: "50%",
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+} );
