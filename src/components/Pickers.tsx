bb80bc00b76f8b750132023d17c6ab999845a811
Jose <joseimascherpa@gmail.com>
2025-04-07 01:40:43 -0300

    Pickers: update options in picker, change types to string, and add StyleSheet

--- src/components/Pickers.tsx
+++ src/components/Pickers.tsx
@@ -1,28 +1,52 @@
 
 import { Picker } from '@react-native-picker/picker';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 interface PickersProps {
-  titlePicker:string;
-  onValueChange: (value: number) => void;
-  value?:number;
+  titlePicker: string;
+  onValueChange: ( value: string ) => void;
+  value?: string;
 }
 
-export const Pickers = ( { titlePicker,onValueChange,value=0}: PickersProps ) => {
+export const Pickers = ( { titlePicker, onValueChange, value = "" }: PickersProps ) => {
+  const optionsPicker = [ "Excelente", "Muy bien", "Bien", "No tan bien", "Mal", "Muy mal" ];
   return (
-    <View style={{marginLeft:30,alignItems:"center",flexDirection:"row"}}>
-      <Text style={{marginRight:10,fontSize:15,fontWeight:"bold"}}>{titlePicker}</Text>
-      <Picker      
-      selectedValue={value}
-      onValueChange={onValueChange}      
-      style={ { borderWidth:1,borderColor:"red", width: "50%", height: "auto", alignSelf: "center" } }
-    >
-      <Picker.Item label="del 1 al 10" value={ 0 } />
-      { Array.from( { length: 10 }, ( _, i ) => i + 1 ).map( ( num ) => (
-        <Picker.Item key={ num } label={ num.toString() } value={ num } />
-      ) ) }
-    </Picker>
+    <View style={ styles.containerPicker }>
+      <Text style={ styles.textTitle }>{ titlePicker }</Text>
+      <Picker
+        selectedValue={ value }
+        onValueChange={ onValueChange }
+        style={ styles.picker }
+      >
+        <Picker.Item label="Seleccione una opción" value="" style={ styles.placeholderPicker } />
+        { optionsPicker.map( ( word, index ) => (
+          <Picker.Item key={ index } label={ word } value={ word } />
+        ) ) }
+      </Picker>
     </View>
 
   );
 };
+
+const styles = StyleSheet.create( {
+  containerPicker: {
+    marginLeft: 15,
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  textTitle: {
+    marginRight: 10,
+    fontSize: 15,
+    fontWeight: "bold"
+  },
+  picker: {
+    borderWidth: 1,
+    flex: 1,
+    height: "auto",
+    alignSelf: "center"
+  },
+  placeholderPicker: {
+    fontSize: 15
+  }
+
+} );
